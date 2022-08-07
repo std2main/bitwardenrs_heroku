d77161709fefc31763c51eff7a4540341bfd7316
OldTyT <nezlobin118@gmail.com>
2022-08-07 22:19:06 +0800

    Update repo

--- solutions/git_store/inotify_backup_db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /data
-# TODO: watching is not working now.
-inotifywait -mrq --format '%w%f' -e modify "/data/db.sqlite3"  | while read line  
-do
-  echo "DB change detected: ${line}"
-  /git_backup.sh backup_db
-done
